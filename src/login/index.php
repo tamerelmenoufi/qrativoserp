7b5d7b80a95c4144eff7f03f3cf485a80094f2fc
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-19 22:48:23 -0400

    Instalação

--- src/login/index.php
+++ src/login/index.php
@@ -14,21 +14,21 @@
         if(mysqli_num_rows($result)){
             $d = mysqli_fetch_object($result);
             $_SESSION['QrAtivosLogin'] = $d->codigo;
-            $retorno = "{
-                'sucesso' : true,
-                'QrAtivosLogin' : $d->codigo,
-                'MaterConnectado' : {$_POST['MaterConnectado']},
-                'msg' : 'Login Realizado com sucesso'
-            }";
+            $retorno = [
+                'sucesso' => true,
+                'QrAtivosLogin' => $d->codigo,
+                'MaterConnectado' => $_POST['MaterConnectado'],
+                'msg' => 'Login Realizado com sucesso'
+            ];
         }else{
-            $retorno = "{
-                'sucesso' : false,
-                'QrAtivosLogin' : false,
-                'MaterConnectado' : false,
-                'msg' : 'Ocorreu um erro no seu login'
-            }";
+            $retorno = [
+                'sucesso' => false,
+                'QrAtivosLogin' => false,
+                'MaterConnectado' => false,
+                'msg' => 'Ocorreu um erro no seu login'
+            ];
         }
-        echo $retorno;
+        echo json_encode($retorno);
         exit();
     }
 ?>
@@ -174,12 +174,13 @@
             $.ajax({
                 url:"src/login/index.php",
                 type:"POST",
-                dataType:"json",
+                // dataType:"json",
                 data:{
                     acao:'login'
                 },
                 success:function(dados){
-                    $.alert(dados);
+                    let retorno = JSON.parse(dados);
+                    $.alert(retorno.sucesso);
                 }
             });
         };
