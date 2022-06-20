8f3631ca1ec93e587522b4b61d1d15696f0ecab0
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-19 22:42:44 -0400

    Instalação

--- src/login/index.php
+++ src/login/index.php
@@ -14,19 +14,19 @@
         if(mysqli_num_rows($result)){
             $d = mysqli_fetch_object($result);
             $_SESSION['QrAtivosLogin'] = $d->codigo;
-            $retorno = [
+            $retorno = [[
                 'sucesso' => true,
                 'QrAtivosLogin' => $d->codigo,
                 'MaterConnectado' => $_POST['MaterConnectado'],
                 'msg' => 'Login Realizado com sucesso'
-            ];
+            ]];
         }else{
-            $retorno = [
+            $retorno = [[
                 'sucesso' => false,
                 'QrAtivosLogin' => false,
                 'MaterConnectado' => false,
                 'msg' => 'Ocorreu um erro no seu login'
-            ];
+            ]];
         }
         echo json_encode($retorno);
         exit();
