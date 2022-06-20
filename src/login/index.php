2b911b602872edd4319b4d2759e997ac3fea880b
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-19 22:17:07 -0400

    Instalação

--- src/login/index.php
+++ src/login/index.php
@@ -6,7 +6,7 @@
         $senha = md5($_POST['senha']);
         $query = "select * from usuarios where login = '{$login}' and senha = '{$senha}'";
         $result = mysqli_query($conn, $query);
-        if(mysqli_num_rows()){
+        if(mysqli_num_rows($result)){
             $d = mysqli_fetch_object($result);
             $_SESSION['QrAtivosLogin'] = $d->codigo;
             $retorno = [
