85c0a1a91dc68624d2743a055b91c60f5c59b43a
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-24 17:59:38 -0400

    Atualizações

--- src/componentes/menu_topo/menu.php
+++ src/componentes/menu_topo/menu.php
@@ -11,9 +11,7 @@
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <img src="img/logo_h60.png" style="height:40px; margin-right:20px;" alt="Sistema de Gestão QrAtivos">
-    <button class="btn">
-      <i class="fa-solid fa-bars"></i>
-    </button>
+    <i class="fa-solid fa-bars"></i>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
