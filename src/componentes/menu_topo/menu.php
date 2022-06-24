87f0eef7f3a8a04b2ee01875d7fd2f15b8895e1d
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-24 11:50:32 -0400

    Atualizações

--- src/componentes/menu_topo/menu.php
+++ src/componentes/menu_topo/menu.php
@@ -32,7 +32,7 @@
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
-        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+        <ul class="navbar-nav">
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Link
