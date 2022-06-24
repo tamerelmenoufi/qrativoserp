cb77538048b11ca36299e8e3d8120cb2d23a7171
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-24 11:54:03 -0400

    Atualizações

--- src/home/index.php
+++ src/home/index.php
@@ -1,16 +1,17 @@
-<div id="paginaHome"></div>
+<div id="paginaHomeTopo"></div>
+<div id="paginaHomeLateral"></div>
 <script>
     $(function(){
         pags = [
-            'src/componentes/menu_topo/menu.php',
-            'src/componentes/menu_lateral/menu.php',
+            ['src/componentes/menu_topo/menu.php','paginaHomeTopo'],
+            ['src/componentes/menu_lateral/menu.php','paginaHomeLateral'],
             ];
 
         for(i=0;i<pags.length;i++){
             $.ajax({
-                url:pags[i],
+                url:pags[i][0],
                 success:function(dados){
-                    $("#paginaHome").append(dados);
+                    $(`#${pags[i][1]}`).append(dados);
                 }
             });
         }
