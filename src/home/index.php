88fa68f45472e331353edcb721d0f7c2d8175260
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-24 11:56:04 -0400

    Atualizações

--- src/home/index.php
+++ src/home/index.php
@@ -11,7 +11,7 @@
             $.ajax({
                 url:pags[i][0],
                 success:function(dados){
-                    $(`#${pags[i][1]}`).append(dados);
+                    $(`#${pags[i][1]}`).html(dados);
                 }
             });
         }
