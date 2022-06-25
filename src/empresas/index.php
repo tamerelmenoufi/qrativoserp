d1f4efcb92cb17bec5205a788565adee7f8a9059
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-25 01:23:30 -0400

    Atualizações

--- src/empresas/index.php
+++ src/empresas/index.php
@@ -3,10 +3,10 @@
         <h5>Gerenciamento de Empresas</h5>
         <div class="d-flex align-items-start">
         <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-            <a class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Emresas</a>
-            <a class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false" disabled>Dados da empresa</a>
-            <a class="nav-link" id="v-pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#v-pills-disabled" role="tab" aria-controls="v-pills-disabled" aria-selected="false" disabled>Contatos / Representantes</a>
-            <a class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false" disabled>Endereços / Filiais</a>
+            <a class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true"><i class="fa-solid fa-building"></i> Emresas</a>
+            <a class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false" disabled><i class="fa-solid fa-list-check"></i> Dados da empresa</a>
+            <a class="nav-link" id="v-pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#v-pills-disabled" role="tab" aria-controls="v-pills-disabled" aria-selected="false" disabled><i class="fa-solid fa-id-badge"></i> Contatos / Representantes</a>
+            <a class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false" disabled><i class="fa-solid fa-location-dot"></i> Endereços / Filiais</a>
         </div>
         <div class="tab-content" id="v-pills-tabContent">
             <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">...</div>
