827064e745b7812d5c6280b8b6c97eca02483538
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-24 22:55:47 -0400

    Atualizações

--- src/componentes/menu_lateral/menu.php
+++ src/componentes/menu_lateral/menu.php
@@ -4,6 +4,7 @@
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
+    <img src="img/logo_h60.png" style="height:60px;" alt="Sistema de Gestão QrAtivos">
     <div>
       Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
     </div>
