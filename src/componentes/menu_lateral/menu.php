a3996e7e303a85c1eb799369e3a07332fd9e8c96
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-24 23:16:08 -0400

    Atualizações

--- src/componentes/menu_lateral/menu.php
+++ src/componentes/menu_lateral/menu.php
@@ -7,7 +7,7 @@
     <h5>Sistema de Gestão QrAtivos</h5>
     <div class="row">
       <div class="col">
-        <a href="#"><i class="fa-solid fa-clipboard-list"></i> Controle de Ordem de Serviços</a>
+        <a href="#" class="text-decoration-none"><i class="fa-solid fa-clipboard-list"></i> Controle de Ordem de Serviços</a>
       </div>
     </div>
     <div class="row">
