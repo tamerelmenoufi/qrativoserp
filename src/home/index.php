ceff42a14f7b21d625175a8b3749d901e018cda3
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-25 17:10:27 -0400

    Atualizações

--- src/home/index.php
+++ src/home/index.php
@@ -20,6 +20,7 @@
         pags = [
             ['src/componentes/menu_topo/menu.php','paginaHomeTopo'],
             ['src/componentes/menu_lateral/menu.php','paginaHomeLateral'],
+            ['src/componentes/menu_lateral_direita/menu.php','paginaHomeLateralDireita'],
             ['src/home/dashboard/index.php','paginaHome'],
             ];
 
