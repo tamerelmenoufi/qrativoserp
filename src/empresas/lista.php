1d832bd5110ed7317b1c7da275bdadf29f526f7f
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-25 15:59:35 -0400

    Atualizações

--- src/empresas/lista.php
+++ src/empresas/lista.php
@@ -1,43 +1,31 @@
-<table id="example" class="display" style="width:100%">
-        <thead>
-            <tr>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </thead>
-        <tfoot>
-            <tr>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </tfoot>
-    </table>
+<?php
 
-    <script>
-        $(document).ready(function () {
-            $('#example').DataTable({
-                processing: true,
-                serverSide: true,
-                ajax: {
-                    url: 'src/empresas/post.php',
-                    type: 'POST',
-                },
-                columns: [
-                    { data: 'first_name' },
-                    { data: 'last_name' },
-                    { data: 'position' },
-                    { data: 'office' },
-                    { data: 'start_date' },
-                    { data: 'salary' },
-                ],
-            });
-        });
-    </script>
+?>
+<table id="example" class="display" style="width:100%">
+    <thead>
+        <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Position</th>
+            <th>Office</th>
+            <th>Start date</th>
+            <th>Salary</th>
+        </tr>
+    </thead>
+    <tfoot>
+        <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Position</th>
+            <th>Office</th>
+            <th>Start date</th>
+            <th>Salary</th>
+        </tr>
+    </tfoot>
+</table>
+
+<script>
+    $(document).ready(function () {
+
+    });
+</script>
