75f3cc31736e699f38fc144a21309f7042e8c0ad
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-24 22:48:14 -0400

    Atualizações

--- src/componentes/menu_lateral/menu.php
+++ src/componentes/menu_lateral/menu.php
@@ -2,25 +2,7 @@
   <div class="m-3">
 
   <h4>Título da página</h4>
-    <div class="input-group mb-3">
-      <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
-      <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-        <li><hr class="dropdown-divider"></li>
-        <li><a class="dropdown-item" href="#">Separated link</a></li>
-      </ul>
-      <input type="text" class="form-control" aria-label="Text input with dropdown button">
-    </div>
 
-
-    <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
-      Link with href
-    </a>
-    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-      Button with data-bs-target
-    </button>
 
     <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
       <div class="offcanvas-header">
