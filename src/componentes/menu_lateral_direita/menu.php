f96d5dd2401a43f61fa983177549cb2846eadf17
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-25 17:20:19 -0400

    Atualizações

--- src/componentes/menu_lateral_direita/menu.php
+++ src/componentes/menu_lateral_direita/menu.php
@@ -1,7 +1,7 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/sis/lib/includes.php");
 ?>
-<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasDireita" aria-labelledby="offcanvasDireitaLabel">
+<div class="offcanvas offcanvas-end" data-bs-backdrop="static" tabindex="-1" id="offcanvasDireita" aria-labelledby="offcanvasDireitaLabel">
   <div class="offcanvas-header">
     <img src="img/logo_h60.png" style="height:60px;" alt="Sistema de Gestão QrAtivos">
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
