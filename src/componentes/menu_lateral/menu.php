353eed732a1c772070169b3bbc2a9c60a9cb76a1
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-24 22:56:44 -0400

    Atualizações

--- src/componentes/menu_lateral/menu.php
+++ src/componentes/menu_lateral/menu.php
@@ -1,10 +1,9 @@
 <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
   <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+    <img src="img/logo_h60.png" style="height:60px;" alt="Sistema de Gestão QrAtivos">
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
-    <img src="img/logo_h60.png" style="height:60px;" alt="Sistema de Gestão QrAtivos">
     <div>
       Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
     </div>
