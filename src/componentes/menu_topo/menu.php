998413a316354b1c877f9a8a164477c3065bd00f
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-24 22:29:36 -0400

    Atualizações

--- src/componentes/menu_topo/menu.php
+++ src/componentes/menu_topo/menu.php
@@ -8,7 +8,7 @@
 
 
 
-<nav class="navbar navbar-expand-lgXXX navbar-dark bg-dark">
+<nav class="navbar navbar-expand navbar-dark bg-dark">
   <div class="container-fluid">
     <img src="img/logo_h60.png" style="height:40px; margin-right:20px;" alt="Sistema de Gestão QrAtivos">
     <i class="fa-solid fa-bars"></i>
