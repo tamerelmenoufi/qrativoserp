e02d7fe6ab77796b2e6cbc4b803400074558f4a6
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-25 16:44:43 -0400

    Atualizações

--- src/empresas/index.php
+++ src/empresas/index.php
@@ -6,20 +6,23 @@
         border:solid 1px red;
         width:100%;
     }
+    .nav-link{
+        font-weight:nowrap;
+    }
 </style>
 <div class="col">
     <div class="m-3">
         <h5>Gerenciamento de Empresas</h5>
         <div class="d-flex align-items-start">
-        <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-            <a class="nav-link active"><i class="fa-solid fa-building"></i> Emresas</a>
-            <a class="nav-link"><i class="fa-solid fa-list-check"></i> Dados da empresa</a>
-            <a class="nav-link"></i> Contatos / Representantes</a>
-            <a class="nav-link"><i class="fa-solid fa-location-dot"></i> Endereços / Filiais</a>
-        </div>
-        <div class="tab-content">
-            <div class="tab-pane fade show active">...</div>
-        </div>
+            <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <a class="nav-link active"><i class="fa-solid fa-building"></i> Emresas</a>
+                <a class="nav-link"><i class="fa-solid fa-list-check"></i> Dados da empresa</a>
+                <a class="nav-link"></i> Contatos / Representantes</a>
+                <a class="nav-link"><i class="fa-solid fa-location-dot"></i> Endereços / Filiais</a>
+            </div>
+            <div class="tab-content">
+                <div class="tab-pane fade show active">...</div>
+            </div>
         </div>
     </div>
 </div>
