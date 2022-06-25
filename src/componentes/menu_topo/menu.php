5130606cc7bd86c0ca45dcc63558e92008b17d24
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-24 20:46:37 -0400

    Atualizações

--- src/componentes/menu_topo/menu.php
+++ src/componentes/menu_topo/menu.php
@@ -18,7 +18,7 @@
 
     <div class="collapse navbar-collapse" id="navbarScroll">
 
-      <ul class="navbar-nav" style="--bs-scroll-height: 100px;">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
         <li class="nav-item">
         </li>
       </ul>
