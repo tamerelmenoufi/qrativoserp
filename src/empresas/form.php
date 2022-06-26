778039703a1a2c0f2b1a62b2d49ab15cb63b2156
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-25 23:29:28 -0400

    Atualizações

--- src/empresas/form.php
+++ src/empresas/form.php
@@ -25,7 +25,7 @@
 
 
 
-    $query = "select * from empresas where codigo = '{$_POST['codigo']}'";
+    echo $query = "select * from empresas where codigo = '{$_POST['codigo']}'";
     $result = mysqli_query($con, $query);
     $d = mysqli_fetch_object($result);
 
