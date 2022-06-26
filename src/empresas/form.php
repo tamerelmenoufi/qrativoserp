3c3210f720249a107f445996aaba21ebca0b5114
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-25 20:47:28 -0400

    Atualizações

--- src/empresas/form.php
+++ src/empresas/form.php
@@ -10,102 +10,24 @@
     }
 </style>
 <h2 class="Topo">Dados do Título do formulário</h2>
-Aqui é o formulário para cadastro e edição das empresas
-
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+<div class="row">
+    <div class="col">
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="cnpj" placeholder="CNPJ">
+            <label for="cnpj">CNPJ</label>
+        </div>
+        <div class="form-floating">
+            <input type="text" class="form-control" id="razao_social" placeholder="Razão Social">
+            <label for="razao_social">Razão Social</label>
+        </div>
+        <div class="form-floating">
+            <select class="form-select" id="situacao" aria-label="Situação">
+                <option value="1">Liberado</option>
+                <option value="2">Bloqueado</option>
+            </select>
+            <label for="situacao">Situação</label>
+        </div>
+        <button class="btn btn-primary" type="button">Salvar</button>
+        <button class="btn btn-danger" type="button">Cancelar</button>
+    </div>
+</div>
