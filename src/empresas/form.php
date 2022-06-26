e8440c75f8daa747c3e28802ed97380c91753a5e
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-25 22:47:23 -0400

    Atualizações

--- src/empresas/form.php
+++ src/empresas/form.php
@@ -2,18 +2,18 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/sis/lib/includes.php");
 
     if($_POST['acao'] == 'salvar'){
+
+        unset($data['codigo']);
+
+        foreach ($data as $name => $value) {
+            $attr[] = "{$name} = '" . mysqli_real_escape_string($con, $value) . "'";
+        }
+        $attr = implode(', ', $attr);
+
         if($_POST['codigo']){
-            $query = "update empresas set
-                                        cnpj='{$_POST['cnpj']}',
-                                        razao_social='{$_POST['razao_social']}',
-                                        situacao='{$_POST['situacao']}'
-                        where codigo = '{$_POST['codigo']}'";
+            $query = "update empresas set {$attr} where codigo = '{$_POST['codigo']}'";
         }else{
-            $query = "insert into from
-                                    cnpj='{$_POST['cnpj']}',
-                                    razao_social='{$_POST['razao_social']}',
-                                    situacao='{$_POST['situacao']}',
-                                    data_cadastro = NOW()";
+            $query = "insert into from data_cadastro = NOW(), {$attr}";
         }
         mysqli_query($con, $query);
         exit();
@@ -27,28 +27,51 @@
 <h2 class="Topo">Dados do Título do formulário</h2>
 <div class="row">
     <div class="col">
-        <div class="form-floating mb-3">
-            <input type="text" class="form-control" id="cnpj" placeholder="CNPJ">
-            <label for="cnpj">CNPJ</label>
-        </div>
-        <div class="form-floating mb-3">
-            <input type="text" class="form-control" id="razao_social" placeholder="Razão Social">
-            <label for="razao_social">Razão Social</label>
-        </div>
-        <div class="form-floating mb-3">
-            <select class="form-select" id="situacao" aria-label="Situação">
-                <option value="1">Liberado</option>
-                <option value="2">Bloqueado</option>
-            </select>
-            <label for="situacao">Situação</label>
-        </div>
-        <button class="btn btn-primary" type="button">Salvar</button>
-        <button class="btn btn-danger" type="button">Cancelar</button>
+        <form id="form-<?= $md5 ?>">
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="cnpj" name="cnpj" placeholder="CNPJ">
+                <label for="cnpj">CNPJ</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="razao_social" name="razao_social" placeholder="Razão Social">
+                <label for="razao_social">Razão Social</label>
+            </div>
+            <div class="form-floating mb-3">
+                <select class="form-select" id="situacao" name="situacao" aria-label="Situação">
+                    <option value="1">Liberado</option>
+                    <option value="2">Bloqueado</option>
+                </select>
+                <label for="situacao">Situação</label>
+            </div>
+            <button salvar class="btn btn-primary" type="button">Salvar</button>
+            <button cancelar class="btn btn-danger" type="button">Cancelar</button>
+        </form>
     </div>
 </div>
 
 <script>
     $(function(){
         $("#cnpj").mask("99.999.999/9999-99");
+
+        $('#form-<?=$md5?>').submit(function (e) {
+            e.preventDefault();
+
+            var codigo = $('#codigo').val();
+            var campos = $(this).serializeArray();
+
+            if (codigo) {
+                campos.push({name: 'codigo', value: codigo})
+            }
+
+            $.ajax({
+                url: 'src/empresas/index.php',
+                data: campos,
+                success: function (dados) {
+
+                }
+            })
+
+
+        });
     })
 </script>
