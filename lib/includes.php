ef60e5a8ab2fdbedddf3282b73d092ffda32e9ff
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-27 08:52:55 -0400

    Atualizações

--- lib/includes.php
+++ lib/includes.php
@@ -1,5 +1,12 @@
 <?php
     session_start();
+    if($_SERVER['SCRIPT_FILENAME'] == '/var/www/html/sis/index.php'){
+        if(!$_SESSION['QrAtivosLogin']){
+            $_SESSION = [];
+            echo "<script>window.location.href='./'</script>";
+            exit();
+        }
+    }
     include("/appinc/connect.php");
     $con = AppConnect();
     $md5 = md5(date("YmdHis"));
