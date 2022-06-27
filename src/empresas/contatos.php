73669f3ac0f82ba82dfcbc352d3eab837d373f76
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-27 10:11:48 -0400

    Atualizações

--- src/empresas/contatos.php
+++ src/empresas/contatos.php
@@ -2,8 +2,63 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/sis/lib/includes.php");
 
     if($_POST['empresa']) $_SESSION['empresa'] = $_POST['empresa'];
-
-
 ?>
 
-Contatos
+<div class="col">
+    <div class="col d-flex justify-content-between">
+        <div class="p-2">Dados da tabel de Empresas (<?=$_SESSION['empresa']?>)</div>
+        <div class="p-2">
+            <button
+                class="btn btn-primary"
+                data-bs-toggle="offcanvas"
+                href="#offcanvasDireita"
+                role="button"
+                aria-controls="offcanvasDireita"
+                novoContatoEmpresa
+            >
+                <i class="fa-solid fa-plus"></i>
+                Novo
+            </button>
+        </div>
+    </div>
+</div>
+<?php
+    $query = "select * from empresas_contatos where empresa = '{$_SESSION['empresa']}' order by nome";
+    $result = mysqli_query($con, $query);
+    while($d = mysqli_fetch_object($result)){
+?>
+<div class="card">
+  <h5 class="card-header"><?=$d->razao_social?></h5>
+  <div class="card-body">
+    <h5 class="card-title"><?=$d->cnpj?></h5>
+    <p class="card-text">Cadastrado em <?=$d->data_cadastro?></p>
+    <a
+      editar="<?=$d->codigo?>"
+      class="btn btn-<?=(($d->situacao == '1')?'primary':'danger')?>"
+      data-bs-toggle="offcanvas"
+      href="#offcanvasDireita"
+      role="button"
+      aria-controls="offcanvasDireita"
+    ><?=$d->situacao_descricao?></a>
+  </div>
+</div>
+<?php
+    }
+?>
+
+<script>
+    $(function(){
+        $("button[novoContatoEmpresa]").click(function(){
+            $.ajax({
+                url:"src/empresas/contatos_form.php",
+                type:"POST",
+                data:{
+                    empresa:'<?=$_SESSION['empresa']?>',
+                },
+                success:function(dados){
+                    $(".LateralDireita").html(dados);
+                }
+            });
+        });
+    })
+</script>
