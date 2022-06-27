83804751f3e206ff965a1ca77c7cd10760620207
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-27 11:27:49 -0400

    Atualizações

--- lib/footer.php
+++ lib/footer.php
@@ -1,6 +1,6 @@
 <script src="lib/vendor/bootstrap-5.2.0/js/bootstrap.bundle.min.js"></script>
 
 <script
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBSnblPMOwEdteX5UPYXf7XUtJYcbypx6w&callback=initMap&v=weekly&language=pt&region=BR"
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBSnblPMOwEdteX5UPYXf7XUtJYcbypx6w&language=pt&region=BR"
     async
 ></script>
