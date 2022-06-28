596170b4bb3c6df5808e5d41b58f14cfc8135813
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-28 17:34:04 -0400

    Atualizações

--- src/componentes/menu_lateral/menu.php
+++ src/componentes/menu_lateral/menu.php
@@ -28,7 +28,7 @@
 
     <div class="row mb-1">
       <div class="col">
-        <a url="src/empresas/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
+        <a url="src/colaboradores/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
           <i class="fa-solid fa-clipboard-list"></i> Colaboradores
         </a>
       </div>
