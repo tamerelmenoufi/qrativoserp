e326345ec40f436c56171b23d7c5dbcd4d343ff5
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-28 23:02:53 -0400

    Atualizações

--- src/colaboradores/index.php
+++ src/colaboradores/index.php
@@ -29,7 +29,7 @@
             <th>Nome</th>
             <th>CPF</th>
             <th>Situação</th>
-            <!-- <th>Ações</th> -->
+            <th></th>
         </tr>
     </thead>
     <tbody>
@@ -40,8 +40,22 @@
             <td><?=$d->nome?></td>
             <td><?=$d->cpf?></td>
             <td><?=$d->situacao?></td>
-            <!-- <td>
-                <button
+            <td>
+
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown button
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+                        <li><a class="dropdown-item active" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    </ul>
+                </div>
+
+                <!-- <button
                     editar="<?=$d->codigo?>"
                     class="btn btn-success btn-xs"
                     data-bs-toggle="offcanvas"
@@ -51,8 +65,8 @@
                 >
                     Ed
                 </button>
-                <button excluir="<?=$codigo?>" class="btn btn-danger btn-xs">ex</button>
-            </td> -->
+                <button excluir="<?=$codigo?>" class="btn btn-danger btn-xs">ex</button> -->
+            </td>
         </tr>
         <?php
         }
