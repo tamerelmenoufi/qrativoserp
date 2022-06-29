9cf2c797bb54e17f1632f9f714ce9f8fb17ea483
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-29 17:02:19 -0400

    Atualizações

--- src/os/fotos.php
+++ src/os/fotos.php
@@ -61,11 +61,9 @@
     </div>
 </div>
 
-<div style="position:absolute; bottom:0; left:0; width:100%; overflow: auto; padding:20px; border:solid 1px red;">
-    <div class="row">
-        <div class="col">
-            <div class="ListarFotos"></div>
-        </div>
+<div class="row">
+    <div class="col">
+        <div class="ListarFotos"></div>
     </div>
 </div>
 
