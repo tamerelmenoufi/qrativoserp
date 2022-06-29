237276ea4a0e219861b0d3baa49169d0c4f4a825
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-29 15:48:15 -0400

    Atualizações

--- src/os/fotos.php
+++ src/os/fotos.php
@@ -6,13 +6,13 @@
     .Foto{
         width:100%;
         height:230px;
-        align-items: center;
-        justify-items: center;
         border:solid 1px red;
     }
     .Foto i{
         font-size:70px;
         color:#eee;
+        align-items: center;
+        justify-items: center;
     }
 </style>
 
