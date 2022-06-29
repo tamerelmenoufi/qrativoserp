fd17459352876599b70d21ed6da784def4e70bd3
tamerelmenoufi <tamer@mohatron.com.br>
2022-06-29 15:42:13 -0400

    Atualizações

--- src/os/fotos.php
+++ src/os/fotos.php
@@ -2,4 +2,22 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/sis/lib/includes.php");
 ?>
 
-Fotos
+<style>
+    .Foto{
+        width:100%;
+        height:230px;
+        text-align:center;
+    }
+    .Foto i{
+        font-size:70px;
+        color:#eee;
+    }
+</style>
+
+<div class="row">
+    <div class="col">
+        <div class="Foto">
+            <i class="fa-solid fa-image"></i>
+        </div>
+    </div>
+</div>
