31ffe2f55990e559fd9c22225a3992547b5960c7
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-01 09:20:27 -0400

    Atualizações

--- src/os/fotos_lista.php
+++ src/os/fotos_lista.php
@@ -9,20 +9,20 @@
     <div class="col">
     <h4>Lista de fotos da OS #<?=$_POST['os']?></h4>
     <?php
-    $query = "select * from os_fotos where cod_os = '{$_POST['os']}'";
+    $query = "select a.*, b.nome as colaborador from os_fotos a left join usuarios b on a.colaborador = b.codigo where a.cod_os = '{$_POST['os']}'";
     $result = mysqli_query($con, $query);
     while($d = mysqli_fetch_object($result)){
     ?>
         <div class="card mt-3">
             <div class="row g-0">
                 <div class="col-md-4">
-                <img src="..." class="img-fluid rounded-start" alt="...">
+                <img src="src/os/fotos/<?="{$d->cod_os}/$d->foto"?>" class="img-fluid rounded-start">
                 </div>
                 <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <h5 class="card-title"><?=$d->titulo?></h5>
+                    <p class="card-text"><?=$d->descricao?></p>
+                    <p class="card-text"><small class="text-muted"><?="{$d->colaborador} em {$d->data_cadastro}"?></small></p>
                 </div>
                 </div>
             </div>
