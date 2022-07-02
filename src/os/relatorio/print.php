a3d932a835c4941f18bd7e65687686f57290a628
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-02 13:39:53 -0400

    Atualizações

--- src/os/relatorio/print.php
+++ src/os/relatorio/print.php
@@ -58,4 +58,5 @@ $opts = array('http' =>
 );
 $context = stream_context_create($opts);
 $result = file_get_contents('http://html2pdf.mohatron.com/', false, $context);
-echo base64_decode($result);
+//echo base64_decode($result);
+echo $html;
